b953e177f643df90ed82f8f07262e19f17e2b9c5
Bogdan Opanchuk <bogdan@opanchuk.net>
2014-01-28 16:14:59 +1100

    inliner: Py3 support in Inliner
    In Py3 the function arguments are arg objects, not Name

--- ast_pe/inliner.py
+++ ast_pe/inliner.py
@@ -24,19 +24,31 @@ class Inliner(ast.NodeTransformer):
     def get_return_var(self):
         return self._return_var
 
-    def visit_Name(self, node):
+    def _visit_local(self, node):
         ''' Replacing known variables with literal values
         '''
         self.generic_visit(node)
-        if node.id in self._locals:
-            if node.id in self._mangled:
-                mangled_id = self._mangled[node.id]
+        is_name = isinstance(node, ast.Name)
+
+        node_id = node.id if is_name else node.arg
+        if node_id in self._locals:
+            if node_id in self._mangled:
+                mangled_id = self._mangled[node_id]
             else:
                 mangled_id = new_var_name(self)
-                self._mangled[node.id] = mangled_id
-            return ast.Name(id=mangled_id, ctx=node.ctx)
+                self._mangled[node_id] = mangled_id
+            if is_name:
+                return ast.Name(id=mangled_id, ctx=node.ctx)
+            else:
+                return ast.arg(arg=mangled_id, annotation=node.annotation)
         else:
             return node
+
+    def visit_arg(self, node):
+        return self._visit_local(node)
+
+    def visit_Name(self, node):
+        return self._visit_local(node)
 
     def visit_Return(self, node):
         ''' Substitute return with return variable assignment + break
@@ -48,6 +60,4 @@ class Inliner(ast.NodeTransformer):
                     targets=[ast.Name(id=self._return_var, ctx=ast.Store())],
                     value=node.value),
                 ast.Break()]
-
-
 
