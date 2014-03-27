302dca69fcc04862973b3393eaa20c938ebb01ba
Bogdan Opanchuk <bogdan@opanchuk.net>
2014-03-27 18:10:19 +1100

    setup: tests pass --- added Py3.4 classifier

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: MIT License",
     ],
