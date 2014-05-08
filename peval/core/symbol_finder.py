e1536e4400f9b74d9873e6cf3942e99950f3242c
Bogdan Opanchuk <bogdan@opanchuk.net>
2014-05-08 17:43:57 +1000

    core/symbol_finder: using the new Walker

--- peval/core/symbol_finder.py
+++ peval/core/symbol_finder.py
@@ -1,79 +1,60 @@
 import ast
 import six
 
+from peval.core.walker import Walker
 
-class SymbolCreationFinder(ast.NodeVisitor):
 
-    def __init__(self):
-        self._locals = set()
-        self._ctxs = (ast.Store, ast.Param) if six.PY2 else ast.Store
-        super(SymbolCreationFinder, self).__init__()
+if six.PY2:
+    STORE_CTXS = (ast.Store, ast.Param)
+else:
+    STORE_CTXS = (ast.Store,)
 
-    def visit_arg(self, node):
-        self.generic_visit(node)
-        self._locals.add(node.arg)
 
-    def visit_Name(self, node):
-        self.generic_visit(node)
-        if isinstance(node.ctx, self._ctxs):
-            self._locals.add(node.id)
+@Walker
+class SymbolCreationFinder:
 
-    def visit_ClassDef(self, node):
-        self.generic_visit(node)
-        self._locals.add(node.name)
+    @staticmethod
+    def visit_arg(node, state, **kwds):
+        state.add(node.arg)
+        return node
 
-    def visit_alias(self, node):
-        self.generic_visit(node)
+    @staticmethod
+    def visit_name(node, state, **kwds):
+        if isinstance(node.ctx, STORE_CTXS):
+            state.add(node.id)
+        return node
 
+    @staticmethod
+    def visit_classdef(node, state, **kwds):
+        state.add(node.arg)
+        return node
+
+    @staticmethod
+    def visit_alias(node, state, **kwds):
         name = node.asname if node.asname else node.name
-
         if '.' in name:
             name = name.split('.', 1)[0]
-
-        self._locals.add(name)
-
-    def get_locals(self):
-        return self._locals
-
-
-class SymbolUsageFinder(ast.NodeVisitor):
-
-    def __init__(self):
-        self._locals = set()
-        super(SymbolUsageFinder, self).__init__()
-
-    def visit_Name(self, node):
-        self.generic_visit(node)
-        if isinstance(node.ctx, ast.Load):
-            self._locals.add(node.id)
-
-    def get_locals(self):
-        return self._locals
+        state.add(name)
+        return node
 
 
 def find_symbol_creations(tree):
     ''' Return a set of all local variable names in ast tree
     '''
-    if isinstance(tree, ast.AST):
-        visitor = SymbolCreationFinder()
-        visitor.visit(tree)
-        return visitor.get_locals()
-    else:
-        result = set()
-        for node in tree:
-            result.update(find_symbol_creations(node))
-        return result
+    return SymbolCreationFinder.inspect(tree, state=set())
+
+
+@Walker
+class SymbolUsageFinder:
+
+    @staticmethod
+    def visit_name(node, state, **kwds):
+        if isinstance(node.ctx, ast.Load):
+            state.add(node.id)
+        return node
 
 
 def find_symbol_usages(tree):
     ''' Return a set of all variables used in ast tree
     '''
-    if isinstance(tree, ast.AST):
-        visitor = SymbolUsageFinder()
-        visitor.visit(tree)
-        return visitor.get_locals()
-    else:
-        result = set()
-        for node in tree:
-            result.update(find_symbol_usages(node))
-        return result
+    return SymbolUsageFinder.inspect(tree, state=set())
