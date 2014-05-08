98e1fa7a15ef11f53712ae8fdbb9078426945cd9
Bogdan Opanchuk <bogdan@opanchuk.net>
2014-05-08 17:44:12 +1000

    components/eliminate: using the new Walker

--- peval/components/eliminate.py
+++ peval/components/eliminate.py
@@ -1,40 +1,40 @@
 import ast
 import copy
 
-from peval.core.visitor import Visitor
+from peval.core.walker import Walker
 
 
-def eliminate(node, _):
-    node = copy.deepcopy(node)
-    visitor = Eliminator()
-    visitor.visit(node)
-    return node, _
+def eliminate(node, constants):
+    return _eliminate.transform(node), constants
 
 
-def replace_node(node, **kwds):
+def replace_fields(node, **kwds):
     new_kwds = dict(ast.iter_fields(node))
     new_kwds.update(kwds)
     return type(node)(**new_kwds)
 
 
-class Eliminator(Visitor):
-    ''' Simplify AST, given information about what variables are known
-    '''
-    def visit_FunctionDef(self, node):
-        ''' Make a call, if it is a pure function,
-        and handle mutations otherwise.
-        Inline function if it is marked with @inline.
-        '''
-        self.generic_visit(node)
-        return replace_node(node, body=_eliminate(node.body))
+@Walker
+def _eliminate(node, **kwds):
+    for attr in ('body', 'orelse'):
+        if hasattr(node, attr):
+            new_list = _filter(getattr(node, attr))
+            kwds = {attr: new_list}
+            node = replace_fields(node, **kwds)
+    return node
 
 
-def _eliminate(node_list):
+def _filter(node_list):
     ''' Dead code elimination - remove "pass", code after return
     '''
-    for i, node in enumerate(list(node_list)):
-        if isinstance(node, ast.Pass) and len(node_list) > 1:
-            node_list.remove(node)
+    if len(node_list) == 1 and isinstance(node_list[0], ast.Pass):
+        return node_list
+
+    new_list = []
+    for i, node in enumerate(node_list):
+        if isinstance(node, ast.Pass):
+            continue
         if isinstance(node, ast.Return):
-            return node_list[:i+1]
-    return node_list
+            return new_list + [node]
+        new_list.append(node)
+    return new_list
