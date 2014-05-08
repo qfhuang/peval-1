0538c23ef9a2d82c17fe0f0908c88f147136ed5d
Bogdan Opanchuk <bogdan@opanchuk.net>
2014-05-08 18:02:14 +1000

    components/while_remover: using the new Walker

--- peval/components/while_remover.py
+++ peval/components/while_remover.py
@@ -1,25 +1,19 @@
 import ast
 import copy
 
-from peval.core.visitor import Visitor
+from peval.core.walker import Walker
 
 
-def while_remover(node, _):
-    node = copy.deepcopy(node)
-    visitor = WhileRemover()
-    visitor.visit(node)
-    return node, _
+def while_remover(node, constants):
+    node = WhileRemover.transform(node)
+    return node, constants
 
 
-class WhileRemover(Visitor):
-    ''' Simplify AST, given information about what variables are known
-    '''
-    def visit_While(self, node):
-        ''' Make a call, if it is a pure function,
-        and handle mutations otherwise.
-        Inline function if it is marked with @inline.
-        '''
-        self.generic_visit(node)
+@Walker
+class WhileRemover:
+
+    @staticmethod
+    def visit_while(node, **kwds):
 
         for idx, e in enumerate(node.body):
             if isinstance(e, ast.Break):
