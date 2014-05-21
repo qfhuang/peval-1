a3e7b4060a3a4e1cd73a4c3dfbdb6045945f9917
Bogdan Opanchuk <bogdan@opanchuk.net>
2014-05-21 23:50:50 +1000

    core/gensym: using per-tag counters to simplify testing

--- peval/core/gensym.py
+++ peval/core/gensym.py
@@ -1,11 +1,23 @@
+from collections import defaultdict
+
 from peval.core.symbol_finder import find_symbol_creations
 
 
 class GenSym(object):
 
-    def __init__(self, taken_names=None, counter=1):
+    def __init__(self, taken_names=None, counters=None):
         self._taken_names = taken_names if taken_names is not None else set()
-        self._counter = counter
+
+        # Keeping per-tag counters affects performance,
+        # but the creation of new names happens quite rarely,
+        # so it is not noticeable.
+        # On the other hand, it makes it easier to compare resulting code with reference code,
+        # since in Py3.4 and later we do not need to mangle True/False/None any more,
+        # so the joint counter would produce different variable names.
+        if counters is None:
+            self._counters = defaultdict(lambda: 1)
+        else:
+            self._counters = counters.copy()
 
     @classmethod
     def for_tree(cls, tree=None):
@@ -13,10 +25,12 @@ class GenSym(object):
         return cls(taken_names=taken_names)
 
     def __call__(self, tag='peval_sym'):
+        counter = self._counters[tag]
         while True:
-            name = '__peval_' + tag + '_' + str(self._counter)
-            self._counter += 1
+            name = '__peval_' + tag + '_' + str(counter)
+            counter += 1
             if name not in self._taken_names:
                 break
+        self._counters[tag] = counter
 
-        return name, GenSym(taken_names=self._taken_names, counter=self._counter)
+        return name, GenSym(taken_names=self._taken_names, counters=self._counters)
